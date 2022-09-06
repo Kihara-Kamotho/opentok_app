e0b0b0753af03eff1e7c859320c7b6f4d3949e1f
Kihara-Kamotho <kiharaalphan@gmail.com>
2022-09-06 13:32:33 +0300

    removed sign in callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,6 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized 
 
   private
-  def after_sign_up_path(user) 
-    redirect_to root_path
-  end
-
-  def after_sign_in_path(user) 
-    redirect_to root_path
-  end 
 
   def user_not_authorized 
     flash[:alert] = "You are not authorized to perform this action"
